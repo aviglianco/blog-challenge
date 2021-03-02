45fa357ec8e21c3fd2066ee9f148fdab2e50ff70
aviglianco <aviglianco22@gmail.com>
2021-03-02 10:39:26 -0300

    Complete pages and routes (challenge 11)

--- app.js
+++ app.js
@@ -10,19 +10,43 @@ const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rho
 
 const app = express();
 
+var postsList = [];
+
 app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
+app.get("/", function(req, res) {
+  res.render("home", {
+    homeStartingContent: homeStartingContent
+  });
+});
 
+app.get("/about", function(req, res) {
+  res.render("about", {
+    aboutContent: aboutContent
+  });
+});
 
+app.get("/contact", function(req, res) {
+  res.render("contact", {
+    contactContent: contactContent
+  });
+});
 
+app.get("/compose", function(req, res) {
+  res.render("compose");
+});
 
-
-
-
-
+app.post("/compose", function(req, res) {
+  const post = {
+    title: req.body.postTitle,
+    content: req.body.newPost
+  }
+  postsList.push(post)
+  res.redirect("/");
+});
 
 
 
